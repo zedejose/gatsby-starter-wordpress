186717a8867ca21d04c88373d7e610645d73461e
Zé <jfontainhas@gmail.com>
2019-03-01 19:21:32 +0000

    Try another WP

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'www.goabstract.com',
+        baseUrl: 'www.wp-portugal.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
