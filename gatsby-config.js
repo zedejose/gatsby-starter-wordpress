6cde54e09fd5dd5b5da28fc0654ef6202b88db91
Zé <jfontainhas@gmail.com>
2019-03-01 19:16:16 +0000

    Test with another live site

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'wpdemo.gatsbycentral.com',
+        baseUrl: 'www.goabstract.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
         protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: false,
+        useACF: true,
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
