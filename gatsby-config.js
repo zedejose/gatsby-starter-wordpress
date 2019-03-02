56002c7ad38be3610fc60778183ed573054bdd15
Zé <jfontainhas@gmail.com>
2019-03-02 15:57:18 +0000

    Try with GraphQL enabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'www.wp-portugal.com',
+        baseUrl: 'ze.fonatinhas.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
         protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: true,
+        useACF: false,
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
